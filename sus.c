15f7b6987bed6220cab52f0b67a2fb91e2e1211a
Wioun <wioun@proton.me>
2024-12-30 09:15:56 +0500

    system() replaced by fork() and execl(), added -v --version, dynamic memory allocation and comments.

--- sus.c
+++ sus.c
@@ -4,58 +4,92 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <errno.h>
-#include <termios.h>
 
-#define DEF_USER "root"
+#define DEFAULT_USER "root"
 
 int main(int argc, char *argv[]) {
-
+        
+    /*| Parsing args |*/ 
     short usr = 0;
-
-    // Parsing args
     if (argc < 2){
+
         printf("Usage: sus [-u user] [command]\n");
         return 1;
     }
     if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-        printf("Execute the command as another user using su.\nUsage: sus [-u user] [command]\n-u --user\tspecify the user.\n-h --help\tshow help message.\n"); 
+
+        printf("Execute the command as another user using su.\nUsage: sus [-u user] [command]\n-u --user\tspecify the user\n-h --help\tdisplay help message\n-v --version\tdisplay version information\n"); 
+        return 0;
+    }
+    if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
+
+        printf("SuS 1.1\nCopyright (C) Wioun 2024, GNU GPL v3.\n"); 
         return 0;
     }
     if (strcmp(argv[1], "-u") == 0 || strcmp(argv[1], "--user") == 0) {
-        usr = 1;
+
         if (argc < 4) {
-            printf("Syntax error.\nUsage: sus [-u user] [command]\n");
+            
+            printf("Usage: sus [-u user] [command]\n");
             return 1;
         }
-    }
-	
-    // Command
-    char cmd[1024] = "";
-
-    short usarg; // count of arguments to specify a user ('-u user' (+2) or nothing (+0))
-    if (usr == 0) usarg = 1;
-    else usarg = 3; 
-    for (int i = usarg; i < argc; i++) {
-        if (i > usarg) {
-            strcat(cmd, " "); 
-        }
-        strcat(cmd, argv[i]); 
+		usr = 1;
     }
 
-    char fcmd[1050];
-    if (usr == 0) sprintf(fcmd, "su %s -c '%s'", DEF_USER, cmd);
-    else sprintf(fcmd, "su %s -c '%s'", argv[2], cmd);
-    // printf("'%s' %hd\n", cmd, usr); // for debug
-    	
-    // Execute
-    struct termios oldt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    int result = system(fcmd);
-    if (result == -1) {
-        perror("system");
+    /*| Command |*/
+	short usrarg; // count of arguments to specify a user ('-u user' (+2) or nothing (+0)).
+	if (usr == 0) usrarg = 1; else usrarg = 3; 
+
+	size_t cmdlen; // memory allocation for command.
+    for (int i = usrarg; i < argc; i++) cmdlen += strlen(argv[i]) + 1; // +1 for space.
+	char *cmd = malloc(cmdlen + 1); // +1 for '\0'.
+	if (cmd == NULL) {
+
+        perror("malloc"); 
         exit(EXIT_FAILURE);
     }
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    exit(EXIT_SUCCESS);
+
+	size_t fcmdlen; // memory allocation for full command.
+	if (usr == 0) fcmdlen = cmdlen + strlen("su  -c ''") + strlen(DEFAULT_USER); // two spaces, yes.
+	else fcmdlen = cmdlen + strlen("su  -c ''") + strlen(argv[2]);
+    char *fcmd = malloc(fcmdlen);
+	if (fcmd == NULL) {
+
+        perror("malloc"); 
+        exit(EXIT_FAILURE);
+    }
+
+	cmd[0] = '\0'; // command itself.
+	for (int i = usrarg; i < argc; i++) {
+
+		if (i > usrarg) {
+
+			strcat(cmd, " ");
+		}
+		strcat(cmd, argv[i]);
+	}
+    
+    if (usr == 0) sprintf(fcmd, "su %s -c '%s'", DEFAULT_USER, cmd); // full command.
+	else sprintf(fcmd, "su %s -c '%s'", argv[2], cmd);
+
+    /*| Execute |*/ 
+	pid_t pid = fork();
+    if (pid == -1) {
+
+        perror("fork");
+        return 1;
+    } 
+	else if (pid == 0) {
+
+        // printf("fcmd: '%s'\ncmd: '%s'\nusr: %hd\n", fcmd, cmd, usr); // for debug.
+        execl("/bin/sh", "sh", "-c", fcmd, (char *)NULL);
+		perror("execl");
+		free(cmd);
+        exit(EXIT_FAILURE);
+    } 
+	else {
+
+        wait(NULL);
+    }
     return 0;
 }
